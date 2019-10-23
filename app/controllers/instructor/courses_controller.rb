ecf924a7c0fe84866c670b386b3bc511991bef83
kimberly sanchez <kxmbers22@gmail.com>
2019-10-23 02:11:43 +0000

    lesson 20 missing things?

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,7 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
 
   def new
     @course = Course.new
@@ -10,11 +12,23 @@ class Instructor::CoursesController < ApplicationController
     redirect_to instructor_course_path(@course)
   end
 
-  def show
-    @course = Course.find(params[:id])
+ def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+  
+    helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
